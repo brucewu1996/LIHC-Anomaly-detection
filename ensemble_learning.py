154ab54af92ed58f00e5eaa2804b9d90c66bcfe1
brucewu1996 <pisces226608@gmail.com>
2022-06-01 09:49:01 +0800

    Increase output of single svm in ensemble performance

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import random
 import argparse
+from re import search
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix,precision_score
 import multiprocessing as mp
 
 
@@ -25,14 +26,29 @@ def npv(y_true,y_pred) :
     return npv
 
 class svm_ensemble_learning() :
-    def __init__(self,p_m,n_m,all_m,y_all,r = 1,run = 100,t = 20):
-        self.positive_matrix = p_m
-        self.negative_matrix = n_m
+    def __init__(self,all_m,y_all,r = 1,run = 1000,t = 20):
         self.overall_matrix = all_m
         self.y_overall = y_all
         self.ratio = r
         self.run = run
         self.threads = t
+        pos_idx = self.y_overall == 1
+        n_idx = self.y_overall == 0
+        self.positive_matrix = self.overall_matrix[pos_idx,:]
+        self.negative_matrix = self.overall_matrix[n_idx,:]
+
+    def data_split(self) :
+        '''
+        exp_df : data frame of expression profile
+        y : numpy array ,annotation of exp_df, must be binary
+        '''
+        exp_df = self.overall_matrix
+        y = self.y_overall
+        pos_idx = y == 1
+        n_idx = y == 0
+        self.positive_matrix = exp_df[pos_idx,:]
+        self.negative_matrix = exp_df[n_idx,:]
+        
         
     def single_svm(self,n) :
         population = range(self.negative_matrix.shape[0])
@@ -47,7 +63,10 @@ class svm_ensemble_learning() :
         svm = SVC(kernel='linear')
         svm.fit(x_train,y_train)
         y_pred_overall = svm.predict(self.overall_matrix)
-        return y_pred_overall
+        y_pred = svm.predict(x_test)
+        precision = precision_score(y_test,y_pred)
+
+        return y_pred_overall,precision
 
     def ensemble_svm(self) :
         pool = mp.Pool(self.threads)
@@ -74,32 +93,42 @@ def main() :
     parser.add_argument("-r", "--run",type=int,help="number of svm for an ensemble model")
     parser.add_argument("-t", "--threads",type=int,help="number of threads to use")
     parser.add_argument("-o","--output_path",help = 'path of ensemble model output')
+    parser.add_argument("-p","--prefix",type=str,help="prefix of metric output file")
+    parser.add_argument("-s","--stand",type=bool,default=False,help = "Standardziation of not,if standardzied choose False")
     args = parser.parse_args()
-
 
     hbv_conversion_table = pd.read_csv(args.conversion_table,sep = '\t',index_col=0)
     exp_df = pd.read_csv(args.input,sep = '\t',index_col=0)
-    standard_exp = standardization(exp_df)
+    if args.stand :
+        standard_exp = standardization(exp_df)
+        standard_exp = standard_exp.T
+    else :
+        standard_exp = exp_df.to_numpy()
     ### subset by hbv associated gene
     ens_id = hbv_conversion_table['Ensembl_ID'].values       
     hbv_idx = [x.split('.')[0] in ens_id for x in exp_df.index]
-    hbv_exp = standard_exp.T[hbv_idx,:] 
-    n_idx = [x == False for x in hbv_idx]
-    no_hbv_exp = standard_exp.T[n_idx,:]
+    syn_idx = [bool(search('Synthesized',x)) for x in exp_df.index]
 
     y_overall = np.zeros(exp_df.shape[0])
     y_overall[hbv_idx] = 1
+    y_overall[syn_idx] = 1
+
     n_run = args.run
     n_thread = args.threads
-    print(exp_df.shape)
-    ensemble = svm_ensemble_learning(hbv_exp,no_hbv_exp,standard_exp.T,y_overall,run = n_run,t = n_thread)
+    ensemble = svm_ensemble_learning(standard_exp,y_overall,run = n_run,t = n_thread)
     ###differ positive / negative ratio
     for r in np.arange(5,55,5) :
         ensemble.ratio = r
-        for i in range(10):
-            result = ensemble.ensemble_svm()
-            result = np.sum(result,axis = 0)
-            np.save(args.output_path + 'lihc_ensemble_result_ratio_' +str(r)+'_test_'+ str(i) + '.npy',result)
+        result = ensemble.ensemble_svm()
+        # result
+        vote_r = np.zeros((n_run,exp_df.shape[0]))
+        precision_r = np.zeros(n_run)
+        for i in range(n_run) :
+            vote_r[i,:] = result[i,0]
+            precision_r[i] = result[i,1]
+        vote_result = np.sum(vote_r,axis = 0)
+        np.save(args.output_path + args.prefix +'_vote_np_ratio_'+str(r)+ '.npy',vote_result)
+        np.save(args.output_path + args.prefix +'_precision_np_ratio_'+str(r)+ '.npy',precision_r)
 
 if __name__ == '__main__':
     main()
