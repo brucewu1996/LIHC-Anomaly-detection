ed10f304921a36cfe9e71da561cc0e8bb26b82f3
bruce <brucewu1996@github.com>
2023-12-05 15:47:05 +0800

    20231205

--- manuscript/measure_scRNA_clustering.py
+++ manuscript/measure_scRNA_clustering.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import pickle,random
 from umap import UMAP
-from sklearn.metrics import normalized_mutual_info_score,adjusted_rand_score,silhouette_score
-from sklearn.cluster import KMeans
+import hdbscan
+import scanpy as sc
+import anndata
+from sklearn.metrics import normalized_mutual_info_score,adjusted_rand_score
 
 def metadata_coverter(metadata) :
     converter = {}
@@ -18,15 +20,46 @@ def calculate_umap_clustering_result(exp_matrix,ground_truth) :
         exp_matrix (pd.DataFrame): input expression matrix 
         ground_truth (np.array): metadata of samples
     """    
-    n_cluster = len(set(ground_truth))
-    reducer = UMAP(n_jobs=32)
+    reducer = UMAP(n_jobs=32,n_components=30)
     embedding = reducer.fit_transform(exp_matrix)
-    kmeans_result = KMeans(n_clusters=n_cluster).fit_predict(embedding) # type: ignore
+    hdbscan_labels = hdbscan.HDBSCAN().fit_predict(embedding)
 
     metric_dict = {}
-    metric_dict['ARI'] = adjusted_rand_score(ground_truth,kmeans_result)
-    metric_dict['NMI'] = normalized_mutual_info_score(ground_truth,kmeans_result)
-    metric_dict['SC'] = silhouette_score(embedding,labels=kmeans_result) # type: ignore
+    metric_dict['ARI'] = adjusted_rand_score(ground_truth,hdbscan_labels)
+    metric_dict['NMI'] = normalized_mutual_info_score(ground_truth,hdbscan_labels)
+
+    return metric_dict
+
+def format_scanpy_input(exp_matrix,metadata) :
+    """
+    Args:
+        exp_matrix (pd.DataFrame): gene expression matrix, row is sample ,col is gene 
+        metadata (pd.DataFrame): metadata of sample in expression matrix
+    """    
+    x = exp_matrix.to_numpy()
+    obs = metadata
+    var = pd.DataFrame({'Gene' : exp_matrix.columns},index=exp_matrix.columns)
+    data = anndata.AnnData(x,obs=obs,var=var)
+    return data
+
+def calculate_scanpy_leiden_clustering_result(exp_matrix,metadata,condition) :
+    """
+    Args:
+        exp_matrix (pd.DataFrame): gene expression matrix, row is sample ,col is gene 
+        metadata (pd.DataFrame): metadata of sample in expression matrix
+        condition (str): colname of metadata
+    """    
+    data = format_scanpy_input(exp_matrix,metadata)
+    stage_d = metadata_coverter(metadata[condition])
+    ground_truth = metadata.loc[exp_matrix.index,condition].replace(stage_d).values
+    
+    sc.pp.neighbors(data, n_pcs=30,use_rep='X')
+    sc.tl.umap(data)
+    sc.tl.leiden(data, key_added="Leiden_res")
+
+    metric_dict = {}
+    metric_dict['ARI'] = adjusted_rand_score(ground_truth,data.obs.Leiden_res.values)
+    metric_dict['NMI'] = normalized_mutual_info_score(ground_truth,data.obs.Leiden_res.values)
 
     return metric_dict
 
@@ -54,24 +87,43 @@ def main() :
     vote = pd.read_csv("/home/bruce1996/data/LIHC_anomaly_detection/manuscript/material/ensemble_learning_result/ensemble_hbv_only_np_ratio_35_vote_result.txt",sep='\t',index_col=0)
     voted_gene = list(set(vote.index[vote['Vote'] > 0]).intersection(exp_m.index))
     non_voted_gene = list(set(set(vote.index) - set(voted_gene)).intersection(exp_m.index))
+    ### load hvg information
+    seurat_hvg = pd.read_csv("/home/bruce1996/data/LIHC_anomaly_detection/manuscript/material/scRNA_seurat/seurat_hvg.txt",sep='\t',index_col=0)
+    candidate_gene = list(set(seurat_hvg['EnsID'][np.where(seurat_hvg['EnsID'] != 'Non-coding',True,False)]).intersection(exp_m.index))
+    non_candidate_gene = list(set(set(vote.index) - set(voted_gene)).intersection(exp_m.index))
 
-    condition = 'patient'
-    stage_d = metadata_coverter(metadata[condition])
-    y_true = metadata.loc[exp_m.columns,condition].replace(stage_d).values
-    ### calculate random sample clustering result
-    n_iteration = 100
-    metric_df = pd.DataFrame(np.zeros([n_iteration,3]),columns=['ARI','NMI','SC'])
+    for condition in ['virus'] :
+        stage_d = metadata_coverter(metadata[condition])
+        y_true = metadata.loc[exp_m.columns,condition].replace(stage_d).values
+        ### calculate random sample clustering result
+        n_iteration = 100
+        metric_df = pd.DataFrame(np.zeros([n_iteration,2]),columns=['ARI','NMI'])
 
-    for idx in range(n_iteration) :
-        print('%d st iteration for random sampling UMAP clustering' % idx)
-        candidate = random.sample(non_voted_gene,len(voted_gene))
-        input = exp_m.loc[candidate,:]
-        measurement = calculate_umap_clustering_result(input.T,y_true)
-        metric_df.iloc[idx,0] = measurement['ARI'] 
-        metric_df.iloc[idx,1] = measurement['NMI']
-        metric_df.iloc[idx,2] = measurement['SC']
+        for idx in range(n_iteration) :
+            print('%d st iteration for random sampling UMAP clustering' % (idx+1))
+            candidate = random.sample(non_candidate_gene,len(candidate_gene))
+            input = exp_m.loc[candidate,:]
+            try :
+                measurement = calculate_umap_clustering_result(input.T,y_true)
+                metric_df.iloc[idx,0] = measurement['ARI'] 
+                metric_df.iloc[idx,1] = measurement['NMI']
+            except :
+                pass
+            '''
+            leiden_measurement = calculate_scanpy_leiden_clustering_result(input.T,metadata,condition)
+            leiden_df.iloc[idx,0] = leiden_measurement['ARI'] 
+            leiden_df.iloc[idx,1] = leiden_measurement['NMI']
+            '''
+        ### calculate voted gene clustering result
+        measurement = calculate_umap_clustering_result(exp_m.loc[voted_gene,:].T,y_true)
+        metric_df = metric_df.append(measurement,ignore_index=True)
+        '''
+        leiden_measurement = calculate_scanpy_leiden_clustering_result(exp_m.loc[voted_gene,:].T,metadata,condition)
+        leiden_df = leiden_df.append(leiden_measurement,ignore_index=True)
+        '''
 
-    metric_df.to_csv("/home/bruce1996/data/LIHC_anomaly_detection/manuscript/material/scRNA_measurement/%s_random_sampling_cluster_result.txt" % condition,sep='\t')
+        metric_df.to_csv("/home/bruce1996/data/LIHC_anomaly_detection/manuscript/material/scRNA_measurement/%s_seurat_hvg_random_sampling_cluster_result.txt" % condition,sep='\t')
+        #leiden_df.to_csv("/home/bruce1996/data/LIHC_anomaly_detection/manuscript/material/scRNA_measurement/%s_random_sampling_leiden_cluster_result.txt" % condition,sep='\t')
 
 if __name__ == '__main__' :
     main()
--- utils/survival_utils.R
+++ utils/survival_utils.R
@@ -1,14 +1,16 @@
 library(survival)
 library(survminer)
+library(stringr)
+
 
 survival_analysis<-function(survival_info,fig_prefix,km_col='risk',km_labels=c("Low risk", "High risk")){
   
   survival_input = read.table(survival_info,header = T,row.names = 1,sep = '\t')
   # change status info from str into boolean
-  survival_input$Status = survival_input$Status == 'True'
+  survival_input$Status = survival_input$Status == 'Dead'
   survival_input$Stage = factor(survival_input$Stage, levels = c("Stage I","Stage II","Stage III","Stage IV","Not Available"))
   # cox regression by gene expression
-  genes = colnames(survival_input)[6:dim(survival_input)[2]]
+  genes = colnames(survival_input)[8:dim(survival_input)[2]]
   # create a formula
   fmla <- as.formula(paste("Surv(Survival_day, Status) ~", paste(genes, collapse= "+")))
   total_fmla <- as.formula(paste("Surv(Survival_day, Status) ~", paste(colnames(survival_input)[3:dim(survival_input)[2]], collapse= "+")))
@@ -36,6 +38,48 @@ survival_analysis<-function(survival_info,fig_prefix,km_col='risk',km_labels=c("
   return(list(forest_plot,kmplot,summary(model),logrank_pv))
 }
 
+survival_analysis_v2<-function(clinical_info,exp_m,fig_prefix,risk_score_mode=TRUE,confounding_factor = c("Gender","Age","Stage"),
+                               km_col='risk',km_labels=c("Low risk", "High risk")){
+  
+  #clinical_info = read.table(clinical_path,header = T,row.names = 1,sep = '\t')
+  clinical_info = clinical_info[,c("Survival_day","Status",confounding_factor)]
+  #exp_m = read.table(exp_path,header = T,row.names = 1,sep = '\t')
+  # change status info from str into boolean
+  clinical_info$Status = survival_input$Status == 'Dead'
+  #clinical_info$Stage = factor(survival_input$Stage, levels = c("Stage I","Stage II","Stage III","Stage IV","Not Available"))
+  # create a formula for cox regression by gene expression
+  genes = colnames(exp_m)
+  fmla <- as.formula(paste("Surv(Survival_day, Status) ~", paste(genes,collapse= "+")))
+  #merge clinical info & expression matrix
+  survival_input = cbind.data.frame(clinical_info,exp_m)
+  model <- coxph(fmla,data = survival_input[c(genes,"Survival_day","Status")] )
+  risk_score = predict(model, survival_input, type="risk")
+  survival_input$risk = risk_score > 1
+  survival_input$risk_score = risk_score
+  survival_v2 = data.frame(survival_input)
+  # Second stage cox regression
+  if (risk_score_mode){
+    total_fmla <- as.formula(paste("Surv(Survival_day, Status) ~", paste(c(confounding_factor,'risk_score'),collapse= "+")))
+  }else {
+    total_fmla <- as.formula(paste("Surv(Survival_day, Status) ~", paste(c(confounding_factor,genes),collapse= "+")))
+  }
+  model_v2 <- coxph(total_fmla,data = survival_v2)
+  ## Visualization cox result
+  forest_plot = ggforest(model_v2,main=fig_prefix,data=survival_v2)
+  km_fmla = as.formula(paste0("Surv(Survival_day, Status) ~ ",km_col))
+  #fit <- do.call(surfit, args = list(formula = fmla, data = survival_input))
+  km_survival = data.frame(survival_v2)
+  fit = survminer::surv_fit(formula = km_fmla,data = km_survival)
+  kmplot = ggsurvplot(fit, data = km_survival,pval = TRUE,
+                      conf.int = TRUE,
+                      legend.title = str_to_title(km_col),
+                      legend.labs = km_labels,
+                      # Add risk table
+                      risk.table = TRUE,
+                      tables.height = 0.2,
+                      palette = c("#98EECC", "#FEA1A1")) + ggtitle(fig_prefix)
+  return(list(forest_plot,kmplot,summary(model)))
+}
 
 ggsave_workaround <- function(g){survminer:::.build_ggsurvplot(x = g,
                                                                surv.plot.height = NULL,
